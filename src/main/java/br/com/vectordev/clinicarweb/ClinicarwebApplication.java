89936e604a5db24086fa953ac2876fbfce2796bd
Washington Luis Pedrosa Lopes <washlopes@gmail.com>
2022-06-18 20:14:38 -0300

    Criação do projeto

---
+++ src/main/java/br/com/vectordev/clinicarweb/ClinicarwebApplication.java
@@ -0,0 +1,13 @@
+package br.com.vectordev.clinicarweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicarwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicarwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vectordev/clinicarweb/ClinicarwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vectordev.clinicarweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicarwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
